29c0402e908fbec80660b19518981c9ca9ff65d0
Glitch (heartbreaking-wary-iris) <none>
2020-11-02 13:06:17 +0000

    💏🐉 Checkpoint ./server.js:18083771/392 ./config.js:18083771/30

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
- const express = require("express");
+const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
-  res.status(200).send("OK bomer")
-})
+app.get("/", function (req, res) {
+  res.status(200).send({ json: "Hello" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
