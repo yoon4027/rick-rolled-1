507c653c633909aedf7c376edfbdf4bf04e26fd1
Glitch (heartbreaking-wary-iris) <none>
2020-11-02 14:24:51 +0000

    🐬🏜 Checkpoint ./src/API/Api.js:18083771/157 ./src/API/index.html:18083771/817

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,14 +1,15 @@
+const { join } = require("path");
 module.exports = class API {
   start() {
     const { URL } = require("../Config.js");
 const express = require("express");
 const app = express();
 app.get("/", function(req, res){
-    res.status(200).redirect(URL);
+    res.status(200).sendFile(join(__dirname, "index.html"));
 });
 app.get("about", function(req, res){
   res.status(200).json({ "res": "GET RICK ROLLED" })
 });
-// app.listen(process.env.PORT, () => console.log(`API online on ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`API online on ${process.env.PORT}`))
   }
 }
