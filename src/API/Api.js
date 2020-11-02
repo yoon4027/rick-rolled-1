129b61c4b52cef37132158a550db793a5781df14
Glitch (heartbreaking-wary-iris) <none>
2020-11-02 13:28:52 +0000

    🕴🐚 Checkpoint ./package.json:18083771/1379 ./src/API/Api.js:18083771/559 ./src/Config.js:18083771/112

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,1 +1,9 @@
-const express = require("");
+const { URL } = require("../Config.js");
+const express = require("express");
+const app = express();
+app.get("/", function(req, res){
+    res.status(200).redirect(URL);
+});
+app.get("about", function(req, res){
+  res.status(200).json({ "res": "GET RICK ROLLED" })
+})
---
+++ src/Config.js
@@ -0,0 +1,1 @@
+exports.URL = "https://youtu.be/dQw4w9WgXcQ";
